798631a3264c925c5c7a86e8e2958842612d6da0
Bijan Razavi <brazavi@uncc.edu>
2017-04-20 00:41:34 -0400

    Updated selenium test code to get age as well as click on the activity tab and view all matches button. Does not gather any of the activity data yet.

--- sWebCrawler/src/main.py
+++ sWebCrawler/src/main.py
@@ -11,11 +11,22 @@ driver = webdriver.Chrome()
 driver.get("http://www.itftennis.com/juniors/players/player/profile.aspx?playerid=100202212")
 
 content = []
+age = []
+grade = []
 
 content = driver.find_element_by_xpath('//*[@id="PlayerDiv"]/div[1]/div/div[2]/ul/li[1]/strong')
-
+age = driver.find_element_by_xpath('//*[@id="PlayerDiv"]/div[1]/div/div[2]/ul/li[2]/strong')
 content_text = content.text
+age_text = age.text
 print('content_text: ' + content_text)
-# for i 
+print (age_text)
+
+driver.find_element_by_id('__tab_tabActivities').click()
+driver.find_element_by_id('btnViewAll').click()
+
+
+
+
+# for i
 #     print ('content: ' + i.get_attribute('strong'))
 # dig_content = content.fin
