bed15e4cd8453a6388088658a7b912cc609dc8ad
dimanor3 <bijanrazavi15@yahoo.com>
2017-04-23 21:36:33 -0400

    Continued working on gathering data, haven't suceeded yet D;

--- sWebCrawler/src/main.py
+++ sWebCrawler/src/main.py
@@ -27,61 +27,100 @@ age_text = age.text
 print('content_text: ' + content_text)
 print (age_text)
 
-
-
 driver.find_element_by_id('__tab_tabActivities').click()
 driver.find_element_by_id('btnViewAll').click()
-
 
 match = driver.find_elements_by_class_name('ulMatch')
 td20 = driver.find_elements_by_class_name('td-20')
 td30 = driver.find_elements_by_class_name('td-30')
 td120 = driver.find_elements_by_class_name('td-120')
 
-td20_text = []
-td30_text = []
-td120_text = []
+tables = []
+tables = driver.find_elements_by_tag_name('table')
+
+lastRow = []
+
+for table in tables:
+    lastRow.append (table.find_elements_by_xpath('//*[@id="ActivityDiv"]/ul/li[1]/table[1]/tbody/tr[position()=last()]'))
+
+for row in lastRow:
+    print (str (row))
+
 match_text = []
 
-td20_temp = []
-td30_temp = []
-td120_temp = []
-
 temp = ''
-
-start = td20[0].text
-
-numTraversed = 0
-
-len20 = td20.__len__()
-len30 = td30.__len__()
-len120 = td120.__len__()
-
+#
+# td20_text = []
+# td30_text = []
+# td120_text = []
+#
+# td20_temp = []
+# td30_temp = []
+# td120_temp = []
+#
+# start = int (td20[0].text)
+# td20_temp.append(td20[0].text)
+#
+# numTraversed = 1
+#
+# len20 = td20.__len__()
+# len30 = td30.__len__()
+# len120 = td120.__len__()
+#
+# print ('Test 1 ' + str (td20_temp.__contains__(td20[11].text)))
+# print ('Test 2 ' + str (str(td20[11].text).isdecimal() and start <= int(td20[11].text)))
+# print ('Test 3 ' + str (str(td30[11].text) == 'l'))
+# print ('Test 4 ' + str (str(td30[11].text) == 'L'))
+#
+# i = 1
+#
 # if (len20 == len30 == len120):
-#     for i in range (len20):
-#         if ((i + 1) != len20 and start == td20[i + 1].text):
-#             start = td20[i + 1].text
+#     for i in range (1, len20):
+#     while i < len20:
+#         if ((td20[i].text in td20_temp) or str (td20[i].text).isdecimal() and start < int (td20[i].text) or str (td30[i].text) == 'l' or str (td30[i].text) == 'L'):
+#             if (td30[i].text == 'l' or td30[i].text == 'L'):
+#                 td20_temp.append(td20[i].text)
+#                 numTraversed += 1
+#
+#             if (str (td20[i].text).isdecimal()):
+#                 start = int (td20[i].text)
+#             elif (str (td20[i + 1].text).isdecimal()):
+#                 start = int (td20[i + 1].text)
+#             elif (str (td20[i + 2].text).isdecimal()):
+#                 start = int (td20[i + 2].text)
 #
 #             numTraversed -= 1
 #
-#             for i in range (numTraversed):
+#             for j in range (numTraversed):
+#                 # print ('before 1: ' + str (td20_temp.__len__()))
 #                 td20_temp.pop(0)
+#                 # print ('after 1: ' + str (td20_temp.__len__()))
 #
+#             # print ('before 2: ' + str (td20_temp.__len__()))
 #             td20_text.append(td20_temp.pop(0))
+#             # print ('after 2: ' + str (td20_temp.__len__()))
 #
-#             numTraversed = 0
+#             if (td30[i].text != 'l' and td30[i].text != 'L'):
+#                 td20_temp.append(td20[i].text)
+#                 numTraversed = 1
+#                 i += 1
+#             else:
+#                 if (i + 1 != len20):
+#                     td20_temp.append(td20[i + 1].text)
+#                 i += 2
+#                 numTraversed = 1
+#             # print (str (i) + ' is our position! 1')
 #             continue
 #
 #         td20_temp.append(td20[i].text)
+#         # print (str (i) + ' is our position! 2')
 #         numTraversed += 1
-
-for i in range (len20):
-    td20_text.append(td20[i].text)
-    td30_text.append(td30[i].text)
-    td120_text.append(td120[i].text)
-
-for i in range (len20):
-    print (td20_text[i] + ' ' + td30_text[i] + ' ' + td120_text[i])
+#         i += 1
+#
+# print ('Length: ' + str (td20_text.__len__()))
+#
+# for i in td20_text:
+#     print (i)
 
 for i in match:
     temp = str(i.text)
@@ -97,7 +136,9 @@ for i in match_text:
 
 print (x)
 
-driver.close()
+driver.quit()
+
+#driver.close()
 
 # for i
 #     print ('content: ' + i.get_attribute('strong'))
